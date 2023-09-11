1ec8b2a359e0339e674f8ba03fdfa2fcd181473b
Oussama Rbiay <103605850+orbiay@users.noreply.github.com>
2023-09-11 20:13:59 +0100

    Time: 44 ms (5.31%) | Memory: 13.1 MB (7.90%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<string,int> hash;
+        int sum = 0;
+        for (int i = 0;i < s.length();i++)
+        {
+            if (s[i + 1] && s[i] == 'I' and (s[i + 1] == 'V' or s[i + 1] == 'X'))
+            {
+                cout<<"1"<<endl;
+                if (s[i + 1] == 'V')
+                    hash["IV"] += 4;
+                else 
+                    hash["IX"] += 9;
+                i++;
+                continue;
+            }
+            else if (s[i + 1] and s[i] == 'X' and (s[i + 1] == 'L' or s[i + 1] == 'C'))
+            {
+                cout<<"2"<<endl;
+                if (s[i + 1] == 'L')
+                    hash["XL"] += 40;
+                else 
+                    hash["XC"] += 90;
+                i++;
+                continue;
+            }
+            else if (s[i + 1] and s[i] == 'C' and (s[i + 1] == 'D' or s[i + 1] == 'M'))
+            {
+                cout<<"3"<<endl;
+                 if (s[i + 1] == 'D')
+                    hash["CD"] += 400;
+                else 
+                    hash["CM"] += 900;
+                i++;
+                continue;
+            }
+            cout<<"i = "<<i<<endl;
+            if (s[i] == 'I')
+                hash["I"]++;
+            else if (s[i] == 'V')
+                hash["V"] += 5;
+            else if (s[i] == 'X')
+                hash["X"] += 10;
+            else if (s[i] == 'L')
+                hash["L"] += 50;
+            else if (s[i] == 'C')
+                hash["C"] += 100;
+            else if (s[i] == 'D')
+                hash["D"] += 500;
+            else 
+                hash["M"] += 1000;
+        }
+        sum += hash["I"];
+        sum += hash["V"];
+        sum += hash["X"];
+        sum += hash["L"];
+        sum += hash["C"];
+        sum += hash["D"];
+        sum += hash["M"];
+        sum += hash["IV"];
+        sum += hash["IX"];
+        sum += hash["XL"];
+        sum += hash["XC"];
+        sum += hash["CD"];
+        sum += hash["CM"];
+        return sum;
+    }
+};
