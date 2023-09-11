9b01e63838bc8da72b6ff5948bd0cd986f15f867
Oussama Rbiay <103605850+orbiay@users.noreply.github.com>
2023-09-11 13:59:01 +0100

    Time: 0 ms (100.00%) | Memory: 9.9 MB (8.85%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        vector<int> poshash(2002,0);
+        vector<int> neghash(1001,0);
+        vector<int> hash(2002,0);
+        for (int i = 0;i < arr.size();i++)
+        {
+            if (arr[i] < 0)
+                poshash[(arr[i] * -1) + 1001]++;
+            else 
+                poshash[arr[i]]++;
+        }
+        for (int i = 0;i < poshash.size();i++)
+            if (poshash[i] > 0)
+                hash[poshash[i]]++;
+        for (int i = 0; i < hash.size();i++)
+            if (hash[i] >= 2)
+                return false;
+        return true;
+    }
+};
+
+
