eed721ad8819b1d7862ff2f4a9d60d12bc34a884
Oussama Rbiay <103605850+orbiay@users.noreply.github.com>
2024-08-01 17:30:45 +0100

    Time: 8 ms (63.48%) | Memory: 8.8 MB (44.92%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,15 +2,10 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         vector<int> vec(26,0);
-        for(int i = 0; i < s.size();i++)
-        {
-            std::cout << vec[s[i] - 'a'] << std::endl;
+        for(int i = 0; i < s.size();i++)          
             vec[s[i] - 'a'] += 1 ;
-           std::cout << vec[s[i] - 'a'] << "\n";
-        }
         for (int i = 0; i < t.size();i++)
             vec[t[i] - 'a']--;
-        std::cout << vec[0] << "   " << vec[1] << std::endl;
         for(int i = 0; i < vec.size();i++)
         {
            if (vec[i] != 0)
