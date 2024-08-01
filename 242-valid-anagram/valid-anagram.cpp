91d756a839ff90724affac47b2e50ee8a7ad46c5
Oussama Rbiay <103605850+orbiay@users.noreply.github.com>
2024-08-01 17:35:36 +0100

    Time: 8 ms (63.48%) | Memory: 8.8 MB (44.92%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        vector<int> vec(26,0);
+        for(int i = 0; i < s.size();i++)          
+            vec[s[i] - 'a'] += 1 ;
+        for (int i = 0; i < t.size();i++)
+            vec[t[i] - 'a']--;
+        for(int i = 0; i < vec.size();i++)
+        {
+           if (vec[i] != 0)
+                return false;
+        }
+        return true;
     }
 };
