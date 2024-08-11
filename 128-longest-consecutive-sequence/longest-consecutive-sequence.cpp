9a459bc1fed0534d6bc63107648e17e638f294f5
Oussama Rbiay <103605850+orbiay@users.noreply.github.com>
2024-08-11 19:55:28 +0100

    Time: 183 ms (35.22%) | Memory: 76.9 MB (9.10%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -40,7 +40,8 @@ public:
         map<int,int> hash;
         int max = 0;
         int saver = - 1;
-        for (int i = 0;i < nums.size();i++)
+        int n = nums.size();
+        for (int i = 0;i < n;i++)
             hash[nums[i]]++;
         for (map<int,int>::iterator it = hash.begin();it != hash.end();it++)
         {
@@ -49,65 +50,13 @@ public:
                 max++;
             else
             {
-                // if (std::next(it) == hash.end())
 
                 if (max > saver)
                     saver = max;
                 max = 0;
             }
         }
-    //     return saver + 1;
-    // vector<int> hash(*max_element(nums.begin(),nums.end()) + 1,0);
-    // int max = 0;
-    // int saver = -1;
-
-    // for (int i = 0;i < nums.size();i++)
-    //     hash[nums[i]]++;
-    // for (int i = 0 ; i < hash.size();i++)
-    // {
-    //     if (i < hash.size() - 1 and hash[i] != 0 and hash[i + 1] != 0)
-    //         max++;
-    //     else
-    //     {
-    //         if (saver < max)
-    //         {
-    //             saver = max;
-    //             max = 0;
-    //         }
-    //     }
-    // }
     return saver + 1;
     }
 
 };
-// #include <vector>
-// #include <algorithm>
-// #include <iostream>
-// using namespace std;
-
-// class Solution {
-// public:
-//     int longestConsecutive(vector<int>& nums) {
-//         if (nums.empty()) return 0;
-        
-//         sort(nums.begin(), nums.end());
-//         int maxLength = 1;
-//         int currentLength = 1;
-        
-//         for (int i = 1; i < nums.size(); i++) {
-//             if (nums[i] == nums[i - 1]) {
-//                 continue;
-//             }
-//             if (nums[i] == nums[i - 1] + 1) {
-//                 currentLength++;
-//             } else {
-//                 maxLength = max(maxLength, currentLength);
-//                 currentLength = 1;
-//             }
-//         }
-        
-//         maxLength = max(maxLength, currentLength);
-        
-//         return maxLength;
-//     }
-// };
