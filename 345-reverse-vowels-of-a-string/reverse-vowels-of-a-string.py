d615ae28cf26e6fe05546e5cb2f9031ad23d03ca
Oussama Rbiay <103605850+orbiay@users.noreply.github.com>
2024-08-17 19:09:20 +0100

    Time: 143 ms (25.79%) | Memory: 13.7 MB (77.79%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isVowel(self, c):  # Add 'self' as the first parameter
+        if c in 'aeiouAEIOU':  # Simplified condition
+            return True
+        return False
+
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        i = 0
+        j = len(s) - 1
+        s = list(s)  
+        while i < j:
+            print(i,j)
+            if self.isVowel(s[i]) and not self.isVowel(s[j]):
+                j -= 1
+            elif self.isVowel(s[j]) and not self.isVowel(s[i]):
+                i += 1
+            elif self.isVowel(s[i]) and self.isVowel(s[j]):
+                s[i], s[j] = s[j], s[i]
+                i += 1
+                j -= 1
+            else:
+                i += 1
+                j -= 1
+        return ''.join(s)  
