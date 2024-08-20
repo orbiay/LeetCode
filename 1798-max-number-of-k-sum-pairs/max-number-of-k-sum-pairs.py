379a581c13603e627d269428cd95f8083f4420be
Oussama Rbiay <103605850+orbiay@users.noreply.github.com>
2024-08-20 16:20:14 +0100

    Time: 531 ms (21.09%) | Memory: 21.8 MB (13.80%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -6,9 +6,7 @@ class Solution(object):
         :rtype: int
         """
         op = 0
-        # n = len(nums)
         dics = {}
-        i = 0
         for n in nums:
             if n in dics:
                 dics[n] += 1
@@ -16,14 +14,9 @@ class Solution(object):
                 dics[n] = 1
         for n in nums:
             target = k - n
-            # print(target,n)
             if target in dics and dics[target] > 0:
                 if (target == n and dics[n] > 1) or (target != n and dics[n] > 0):
-                    print("i = ",i,n,target)
                     dics[n] -= 1
                     dics[target] -= 1
                     op += 1
-                    print("target ", dics[target]," dics[n] = ",dics[n])
-            i += 1
         return op
-[1,2,3,4]
