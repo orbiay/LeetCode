19c187cd49570ea3b48ff264e216d95682cf7ca1
Oussama Rbiay <103605850+orbiay@users.noreply.github.com>
2024-08-21 10:09:00 +0100

    Time: 115 ms (62.18%) | Memory: 15.5 MB (66.14%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -11,13 +11,7 @@ class Solution(object):
             if (s[i] in "aeiou"):
                 ret += 1
             if i >= k - 1 :
-                # if ret == k:
-                #     return k
-                # else:
                 maxval = max(maxval,ret)
                 if s[i - (k - 1)] in "aeiou":
                     ret -= 1
-                print(s[i],maxval,i - (k - 1),k)
         return maxval
-
-"leetcode"
