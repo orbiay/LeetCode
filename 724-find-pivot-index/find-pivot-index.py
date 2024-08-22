fdab23ca313441ac54f4abbb122ac2654ea290e8
Oussama Rbiay <103605850+orbiay@users.noreply.github.com>
2024-08-22 16:49:12 +0100

    Time: 103 ms (74.79%) | Memory: 12.9 MB (32.59%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -7,9 +7,6 @@ class Solution(object):
         sumofarr = sum(nums)
         n = len(nums)
         i , j  = 0,1
-        s = 0
-        check = -1
-        left,right = 0,0
         sumofarr -= nums[0]
         prevsum = 0
         prevsum += nums[0]
@@ -17,7 +14,6 @@ class Solution(object):
             return 0
         while j < n:
             sumofarr -= nums[j]
-            print(sumofarr,prevsum)
             if sumofarr == prevsum:
                 return j
             prevsum += nums[j]
