4eca8501ecd0067c2480ff1e7838e55a953238b6
Oussama Rbiay <103605850+orbiay@users.noreply.github.com>
2024-08-23 10:23:13 +0100

    Time: 18 ms (78.14%) | Memory: 11.5 MB (95.97%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -5,27 +5,13 @@ class Solution(object):
         :rtype: bool
         """
         dics = {}
-        unique = [0] * 2001 
         for n in arr:
-            if (n < 0):
-                n += 1000
-            elif n > 0:
-                n += 1000
-            unique[n] += 1
-        print(unique)
-        for i in range(len(unique)):
-            if i >= len(unique):
-                break
-            if unique[i] == 0:
-                continue
-            num = unique[i]
-            unique.pop(i)
-            if num in unique:
-                return False
-                
-        return True
-            # if n not in unique:
-            #     unique.append(n)
+            if n in dics:
+                dics[n] += 1
+            else:
+                dics[n] = 1
+        return len(dics) - len(set(dics.values())) == 0
+
         
             
         
