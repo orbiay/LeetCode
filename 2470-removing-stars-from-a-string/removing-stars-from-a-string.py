0df5da778d4a62ee7c184403ea17a3a085e7383e
Oussama Rbiay <103605850+orbiay@users.noreply.github.com>
2024-08-24 10:54:39 +0100

    Time: 259 ms (88.06%) | Memory: 14.5 MB (33.88%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -5,17 +5,23 @@ class Solution(object):
         :rtype: str
         """
         stack = []
-        i = len(s) - 1
-        while i >= 0:
-            stack.append(s[i])
-            i -= 1
-        i = 0
-        ret = ""
-        while i < len(s):
-            char = stack.pop()
-            if (char == "*" and ret != ""):
-                ret = ret[:-1]
+        # n = len(s)
+        # i = n - 1
+        # while i >= 0:
+        #     stack.append(s[i])
+        #     i -= 1
+        # i = 0
+        # ret = ""
+        # while i < n:
+        #     char = stack.pop()
+        #     if (char == "*" and ret != ""):
+        #         ret = ret[:-1]
+        #     else:
+        #         ret += char 
+        #     i += 1
+        for n in s:
+            if n != '*':
+                stack.append(n)
             else:
-                ret += char 
-            i += 1
-        return ret
+                stack.pop()
+        return ''.join(stack)
