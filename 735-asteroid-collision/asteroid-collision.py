d3529d0d3ace14f609778aa09e6c221343d3a23f
Oussama Rbiay <103605850+orbiay@users.noreply.github.com>
2024-08-26 10:25:30 +0100

    Time: 69 ms (38.53%) | Memory: 12.8 MB (70.59%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def asteroidCollision(self, ast):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        listt = []
+        i = 0
+        for n in ast:
+            if n > 0:
+                listt.append(n)
+            else:
+                if i == 0:
+                    listt.append(n)
+                else:
+                    if n < 0 and len(listt):
+                        save = listt.pop()
+                        if save < 0:
+                            listt.append(save)
+                            listt.append(n)
+                            continue
+                        # elif save == -(n):
+                        #     continue
+                        else:
+                            while save > 0 and  save < -(n)  and len(listt):
+                                save = listt.pop()
+                            if save < 0 and n < 0:
+                                listt.append(save)
+                                listt.append(n)
+                            elif (len(listt) ==  0 and save < -(n)):
+                                listt.append(n)
+                            elif save == -(n):
+                                continue
+                            else:
+                                listt.append(save)
+                    else:
+                        listt.append(n)
+            i += 1
+        return listt
+[-2,2,1,-2]
+(0, [])
+(1, [-2])
+(2, [-2, 2])
+(3, [-2, 2, 1])
