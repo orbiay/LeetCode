c0a218401459a6eee5f3295c6c2c53b305c637e2
Oussama Rbiay <103605850+orbiay@users.noreply.github.com>
2024-08-28 11:06:23 +0100

    Time: 101 ms (39.11%) | Memory: 14.6 MB (13.60%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -5,16 +5,16 @@ class Solution(object):
         :rtype: int
         """
         hash = {}
-        key = 0
         liss = []
         for i,n in enumerate(nums):
             if n in hash:
                 hash[n] += 1
             else:
                 hash[n] = 1
-            if hash[n] == 1 and n not in nums[i + 1:]:
-                key = n
-        return key
+        for key in hash:
+            if (hash[key] == 1):
+                return key
+        return 0
 
         
             
